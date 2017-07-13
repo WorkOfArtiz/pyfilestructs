0f0e88334b82ece7dbd745bbba62a5fbcea94a65
Arthur de Fluiter <arghhhhhthur@gmail.com>
2017-07-13 23:03:31 +0200

    added setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyfilestructs',
+  packages = ['filestructs'],
+  version = '0.1',
+  description = 'A file backed dictionairy teeny "library"',
+  author = 'Arthur de Fluiter',
+  author_email = 'arghhhhhthur@gmail.com',
+  url = 'https://github.com/workofartiz/pyfilestructs',
+  download_url = 'https://github.com/workofartiz/pyfilestructs/archive/0.1.tar.gz',
+  keywords = ['filebacked'], 
+  classifiers = [],
+)
